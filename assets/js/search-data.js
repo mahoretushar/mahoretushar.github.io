417d4dbbfb8c74c809418aa498a890f887cb1dcf
mahoretushar <54020003+mahoretushar@users.noreply.github.com>
2026-04-15 14:35:23 +0000

    Deploying to gh-pages from @ mahoretushar/mahoretushar.github.io@86c93bbc6716ee754d217404347fcd3405d6e8a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,8 +52,8 @@ ninja.data = [{
             window.location.href = "/teaching/";
           },
         },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "cv",
+          description: "Curriculum Vitae — Tushar Mahore",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
